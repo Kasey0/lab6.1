c1a9e86280032bf682a489f8579c79e76c923bf1
Bura A.P. Casian-Mihai <cti22a113@cst.uoradea.local>
2023-11-20 18:53:20 +0200

    Initial commit

---
+++ src/com/company/ContBancar.java
@@ -0,0 +1,7 @@
+package com.company;
+
+private class ContBancar {
+    static void deposit()
+
+
+}
---
+++ src/com/company/ContDeEconomii.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class ContDeEconomii {
+
+    static int withdraw() {
+
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Operatia nu poate fi efectuata!");
+    }
+
+}
